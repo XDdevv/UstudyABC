766eaf8f7c41e73c173588aab1ef6c3fc2d0b5ee
DELL <bobir892012@gmail.com>
2024-06-21 17:44:06 +0500

    Books title qushdim

--- app/src/main/java/com/uz/ustudy/ui/books/BookFragment.kt
+++ app/src/main/java/com/uz/ustudy/ui/books/BookFragment.kt
@@ -1,10 +1,8 @@
 package com.uz.ustudy.ui.books
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.uz.ustudy.R
 import com.uz.ustudy.databinding.FragmentBookBinding
 
